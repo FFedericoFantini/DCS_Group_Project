a6032a5b1b745b6692db72bd36613c86826a2502
FFedericoFantini <fedefante2002@gmail.com>
2025-12-01 17:54:27 +0100

    Refactor create_area_zone to include area parameter

--- create_area.py
+++ create_area.py
@@ -1,32 +1,33 @@
 # create_area.py
 
-
-def create_area_zone(sim, meeting_pos,
+def create_area_zone(sim, area, meeting_pos,
                              width=1.0, height=1.0,
-                             color=[0.2, 0.2, 0.2],   # grigio scuro
-                             transparency=0.15):       # quasi opaco
+                             color=[0.2, 0.2, 0.2],   # dark gray
+                             transparency=0.15,       # almost opaque
+                    ):      
     """
-    Crea un rettangolo (cuboid sottile) sotto la Meeting Area.
+    Creates a rectangle (thin cuboid) under the Meeting Area.
     """
 
     x, y, _ = meeting_pos
 
-    # posizione leggermente sotto il dummy
-    z = 0.01  # sopra il pavimento
+    # position slightly below the dummy
+    z = 0.01  # above the floor
 
-    # POSIZIONE SOPRA IL FLOOR
+    # POSITION ABOVE THE FLOOR
     zone = sim.createPureShape(
         0,  # cuboid
         0,  # options
-        [width, height, 0.02],  # molto sottile
-        0   # statico
+        [width, height, 0.02],  # very thin
+        0   # static
     )
     sim.setObjectInt32Param(zone, sim.shapeintparam_respondable, 1)
     sim.setObjectInt32Param(zone, sim.shapeintparam_static, 1)
-    sim.setObjectAlias(zone, "MEETING_AREA_BASE")
+    area_name = "AREA_BASE_" + area
+    sim.setObjectAlias(zone, area_name)
     sim.setObjectPosition(zone, -1, [x, y, z])
 
-    # colore scuro
+    # dark color
     sim.setShapeColor(zone, None, sim.colorcomponent_ambient_diffuse, color)
     sim.setShapeColor(zone, None, sim.colorcomponent_transparency, [transparency])
 
